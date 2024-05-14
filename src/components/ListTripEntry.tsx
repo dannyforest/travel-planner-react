6d1ca2239082c1e5704f983af0bcd25ac96d9a3b
Benaissanabila <benaissa.nabila14@gmail.com>
2024-05-14 15:28:37 -0400

    componenets changes

--- src/components/ListTripEntry.tsx
+++ src/components/ListTripEntry.tsx
@@ -1,24 +1,63 @@
+
+import styled, {keyframes} from 'styled-components';
 interface Props{
     id:string,
     name:string,
+    description:string,
     image:string,
+    handleOpen:()=>void
 }
-export const ListTripEntry=({id,name,image}:Props)=>{
+export const ListTripEntry=({id,name,description,image,handleOpen}:Props)=>{
 
     return(
-        <div
-            style ={{
-                width: '300px',
-                height: '300px',
-                padding:'40px',
-                margin:'30px',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                backgroundImage: `url(/images/${image}.webp)`
+        <ListTripEntryDiv
 
-        }}
-            key={id}>
-            {name}
-        </div>
+            key={id}
+        image={image}
+        onClick={handleOpen}
+        >
+            <Heading>{name}</Heading>
+            <Paragraphe>{description}</Paragraphe>
+        </ListTripEntryDiv>
     )
 }
+interface ListTripEntryProps {
+    image: string;
+}
+const scaleAnimation = keyframes`
+    0% {
+        transform: scale(1);
+    }
+    50% {
+        transform: scale(1.1);
+    }
+    100% {
+        transform: scale(1);
+    }`
+const ListTripEntryDiv = styled.div<ListTripEntryProps>`
+  cursor: pointer;
+  width: 400px;
+  height: 600px;
+    padding:30px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(/images/${(props) => props.image}.webp);
+    transition: transform 0.3s ease-in-out;
+
+    &:hover {
+        animation: ${scaleAnimation} 0.5s ease-in-out;
+    }
+`;
+const Heading = styled.h1`
+  background-color: rgba(128, 128, 128, 0.5); // Transparent grey background
+  color: white; // White text
+  padding: 1rem;
+  border-radius: 4px;
+`;
+const Paragraphe = styled.p`
+    width: 50%;
+    background-color: rgba(128, 128, 128, 0.5); // Transparent grey background
+    color: white; // White text
+    padding: 1rem;
+    border-radius: 4px;
+`
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import {ListTripEntry} from "../components/ListTripEntry";
+import {Box, Modal, Typography} from "@mui/material";
+
+import {useState} from "react";
 interface Trip {
     id: number;
     name: string;
@@ -12,7 +15,7 @@ interface Trip {
 const trips = [
     {
         id: 1,
-        name: "Trip 1",
+        name: "Trip to Spain",
         description: "Trip 1 description",
         date: "2023-05-01",
         location: "Trip 1 location",
@@ -20,16 +23,29 @@ const trips = [
     },
     {
         id: 2,
-        name: "Trip 2",
+        name: "Trip to Japan",
         description: "Trip 2 description",
         date: "2023-05-02",
         location: "Trip 2 location",
         image: "japan"
     }
 ]
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 export const MainScreen = ()=>{
-
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return(
        <div>
           <h1>
@@ -40,11 +56,28 @@ export const MainScreen = ()=>{
                    <ListTripEntry
                        key={trip.id}
                        name={trip.name}
+                       description={trip.description}
                        id={trip.id.toString()}
                        image={trip.image}
+                       handleOpen={handleOpen}
                    />
                ))}
            </ListTrips>
+           <Modal
+               open={open}
+               onClose={handleClose}
+               aria-labelledby="modal-modal-title"
+               aria-describedby="modal-modal-description"
+           >
+               <Box sx={style}>
+                   <Typography id="modal-modal-title" variant="h6" component="h2">
+                       Text in a modal
+                   </Typography>
+                   <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                       Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                   </Typography>
+               </Box>
+           </Modal>
        </div>
 )
 }
