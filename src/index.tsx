2f5fa8fe77bb4f8568b283adc35b4376dd6b7230
felixsim12 <felixsim21@gmail.com>
2024-05-14 13:27:36 -0400

    -

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+]);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
