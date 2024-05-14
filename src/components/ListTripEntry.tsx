68aee641327fae4aff7d7eb2b615e3a0f5262295
mikky <mikkydidi34@gmail.com>
2024-05-14 15:08:48 -0400

    mui install

--- src/components/ListTripEntry.tsx
+++ src/components/ListTripEntry.tsx
@@ -1,3 +1,5 @@
+import styled, {keyframes} from 'styled-components';
+
 interface Props {
     id: string;
     name: string;
@@ -6,14 +8,41 @@ interface Props {
 
 export const ListTripEntry = ({id, name, image}: Props) => {
     return (
-        <div
-            style={{
-                width: '100%',
-                height: '800px',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                backgroundImage: `url(/images/${image}.jpg)`
-            }}
-            key={id}>{name}</div>
-    );
+        <ListTripEntryDiv
+            key={id}
+            image={image}
+        >
+            {name}
+        </ListTripEntryDiv>
+    )
 }
+
+interface ListTripEntryProps {
+    image: string;
+}
+
+const scaleAnimation = keyframes`
+    0% {
+        transform: scale(1);
+    }
+    50% {
+        transform: scale(1.1);
+    }
+    100% {
+        transform: scale(1);
+    }
+`;
+
+const ListTripEntryDiv = styled.div<ListTripEntryProps>`
+  cursor: pointer;
+  width: 100%;
+  height: 500px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(/images/${(props) => props.image}.jpg);
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    animation: ${scaleAnimation} 0.5s ease-in-out;
+  }
+`;
