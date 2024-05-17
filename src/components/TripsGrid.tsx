4e683fa88225cdbd6389059afcc571afd0438086
felixsim12 <felixsim21@gmail.com>
2024-05-17 09:00:38 -0400

    -

--- src/components/TripsGrid.tsx
+++ src/components/TripsGrid.tsx
@@ -1,51 +1,18 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import {DataGrid, GridActionsCellItem, GridColDef, GridRowId, GridRowModes, GridRowModesModel} from '@mui/x-data-grid';
 import {useTripContext} from "../context/TripContext";
 import {UserTrip} from "../models";
-
-const columns: GridColDef[] = [
-    {
-        field: 'id',
-        headerName: 'ID',
-        width: 325,
-    },
-    {
-        field: 'name',
-        headerName: 'Name',
-        width: 150,
-        editable: true,
-    },
-    {
-        field: 'description',
-        headerName: 'Description',
-        width: 300,
-        editable: true,
-    },
-    {
-        field: 'location',
-        headerName: 'Location',
-        width: 175,
-        editable: true,
-    },
-    {
-        field: 'date',
-        headerName: 'Date',
-        editable: true,
-        width: 260
-    },
-    {
-        field: 'image',
-        headerName: 'Image',
-        editable: true,
-        width: 160
-    },
-];
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import {type} from "node:os";
 
 export default function TripsGrid() {
     const {trips} = useTripContext();
 
-    const rows = trips.map(trip => ({
+    const initialRows = trips.map(trip => ({
         id: trip.id,
         name: trip.name,
         description: trip.description,
@@ -53,6 +20,119 @@ export default function TripsGrid() {
         date: trip.date,
         image: trip.image,
     }));
+
+    const [rows, setRows] = React.useState(initialRows);
+    const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
+
+    const columns: GridColDef[] = [
+        {
+            field: 'id',
+            headerName: 'ID',
+            width: 325,
+        },
+        {
+            field: 'name',
+            headerName: 'Name',
+            width: 150,
+            editable: true,
+        },
+        {
+            field: 'description',
+            headerName: 'Description',
+            width: 300,
+            editable: true,
+        },
+        {
+            field: 'location',
+            headerName: 'Location',
+            width: 175,
+            editable: true,
+        },
+        {
+            field: 'date',
+            headerName: 'Date',
+            editable: true,
+            width: 260
+        },
+        {
+            field: 'image',
+            headerName: 'Image',
+            editable: true,
+            width: 160
+        },
+
+        {
+            field: 'actions',
+            type: 'actions',
+            headerName: 'Actions',
+            width: 100,
+            cellClassName: 'actions',
+            getActions: ({ id }) => {
+                const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+                if (isInEditMode) {
+                    return [
+                        <GridActionsCellItem
+                            icon={<SaveIcon />}
+                            label="Save"
+                            sx={{
+                                color: 'primary.main',
+                            }}
+                            onClick={handleSaveClick(id)}
+                        />,
+                        <GridActionsCellItem
+                            icon={<CancelIcon />}
+                            label="Cancel"
+                            className="textPrimary"
+                            onClick={handleCancelClick(id)}
+                            color="inherit"
+                        />,
+                    ];
+                }
+
+                return [
+                    <GridActionsCellItem
+                        icon={<EditIcon />}
+                        label="Edit"
+                        className="textPrimary"
+                        onClick={handleEditClick(id)}
+                        color="inherit"
+                    />,
+                    <GridActionsCellItem
+                        icon={<DeleteIcon />}
+                        label="Delete"
+                        onClick={handleDeleteClick(id)}
+                        color="inherit"
+                    />,
+                ];
+            },
+        },
+    ];
+
+    const handleEditClick = (id: GridRowId) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+    };
+
+    const handleSaveClick = (id: GridRowId) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    };
+
+    const handleDeleteClick = (id: GridRowId) => () => {
+        setRows(rows.filter((row) => row.id !== id));
+    };
+
+    const handleCancelClick = (id: GridRowId) => () => {
+        setRowModesModel({
+            ...rowModesModel,
+            [id]: { mode: GridRowModes.View, ignoreModifications: true },
+        });
+
+        // const editedRow = rows.find((row) => row.id === id);
+        // if (editedRow!.isNew) {
+        //     setRows(rows.filter((row) => row.id !== id));
+        // }
+    };
+
 
     return (
         <Box sx={{ height: 400, width: '100%' }}>
