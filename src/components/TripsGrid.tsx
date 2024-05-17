3bd8e0e05d6b0e16cb0f68ebc187f1d5326b68d6
felixsim12 <felixsim21@gmail.com>
2024-05-17 11:27:14 -0400

    -

--- src/components/TripsGrid.tsx
+++ src/components/TripsGrid.tsx
@@ -7,16 +7,22 @@ import {
     GridRowId,
     GridRowModel,
     GridRowModes,
-    GridRowModesModel
+    GridRowModesModel, GridRowsProp, GridSlots, GridToolbarContainer
 } from '@mui/x-data-grid';
 import {useTripContext} from "../context/TripContext";
 import {UserTrip} from "../models";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import SaveIcon from '@mui/icons-material/Save';
+import AddIcon from '@mui/icons-material/Add';
 import CancelIcon from '@mui/icons-material/Close';
 import {type} from "node:os";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {DataStore} from "@aws-amplify/datastore";
+import Button from "@mui/material/Button";
+import {
+    randomId,
+} from '@mui/x-data-grid-generator';
 
 interface TripRow {
     id: string;
@@ -26,6 +32,34 @@ interface TripRow {
     date: string | null | undefined;
     image: string | null | undefined;
     isNew?: boolean;
+}
+
+interface EditToolbarProps {
+    setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+    setRowModesModel: (
+        newModel: (oldModel: GridRowModesModel) => GridRowModesModel,
+    ) => void;
+}
+
+function EditToolbar(props: EditToolbarProps) {
+    const { setRows, setRowModesModel } = props;
+
+    const handleClick = () => {
+        const id = randomId();
+        setRows((oldRows) => [...oldRows, { id, name: '', age: '', isNew: true }]);
+        setRowModesModel((oldModel) => ({
+            ...oldModel,
+            [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+        }));
+    };
+
+    return (
+        <GridToolbarContainer>
+            <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+                Add record
+            </Button>
+        </GridToolbarContainer>
+    );
 }
 
 export default function TripsGrid() {
@@ -40,8 +74,20 @@ export default function TripsGrid() {
         image: trip.image,
     }));
 
-    const [rows, setRows] = useState<TripRow[]>(initialRows);
+    const [rows, setRows] = useState<TripRow[]>([]);
     const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
+
+    useEffect(() => {
+        const initialRows: TripRow[] = trips.map(trip => ({
+            id: trip.id,
+            name: trip.name,
+            description: trip.description,
+            location: trip.location,
+            date: trip.date,
+            image: trip.image,
+        }));
+        setRows(initialRows);
+    }, [trips, setRows]);
 
     const columns: GridColDef[] = [
         {
@@ -139,11 +185,16 @@ export default function TripsGrid() {
         setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
     };
 
-    const handleSaveClick = (id: GridRowId) => () => {
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    const handleSaveClick = (id: GridRowId) => async () => {
+        setRowModesModel({...rowModesModel, [id]: {mode: GridRowModes.View}});
     };
 
-    const handleDeleteClick = (id: GridRowId) => () => {
+    const handleDeleteClick = (id: GridRowId) => async () => {
+        const toDelete = await DataStore.query(UserTrip, id.toString());
+        if (toDelete) {
+            await DataStore.delete(toDelete);
+        }
+
         setRows(rows.filter((row) => row.id !== id));
     };
 
@@ -159,7 +210,41 @@ export default function TripsGrid() {
         }
     };
 
-    const processRowUpdate = (newRow: GridRowModel) => {
+    const addOrUpdateTrip = async (newRow: GridRowModel) => {
+        const id = newRow.id.toString();
+
+        try {
+            if (newRow.isNew) {
+                delete newRow.isNew;
+                const userTrip = await DataStore.save(new UserTrip(newRow));
+                console.log(userTrip);
+            } else {
+                const original = await DataStore.query(UserTrip, id.toString());
+                const updatedRow = rows.find((row) => row.id === id);
+
+                if (original) {
+                    const updatedUserTrip = await DataStore.save(
+                        UserTrip.copyOf(original, updated => {
+                            updated.image = updatedRow?.image;
+                            updated.name = updatedRow?.name;
+                            updated.description = updatedRow?.description;
+                            updated.location = updatedRow?.location;
+                            updated.date = updatedRow?.date;
+                            // updated._version = updatedRow?._version;
+                        })
+                    );
+
+                    console.log(updatedUserTrip);
+                }
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const processRowUpdate = async (newRow: GridRowModel) => {
+        await addOrUpdateTrip(newRow);
+
         const updatedRow = { ...newRow, isNew: false } as TripRow;
         setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
         return updatedRow;
@@ -168,6 +253,8 @@ export default function TripsGrid() {
     const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
         setRowModesModel(newRowModesModel);
     };
+
+    console.log(rows);
 
     return (
         <Box sx={{ height: 400, width: '100%' }}>
@@ -184,6 +271,17 @@ export default function TripsGrid() {
                 pageSizeOptions={[5]}
                 checkboxSelection
                 disableRowSelectionOnClick
+                editMode="row"
+                rowModesModel={rowModesModel}
+                onRowModesModelChange={handleRowModesModelChange}
+                onRowEditStop={handleRowEditStop}
+                processRowUpdate={processRowUpdate}
+                slots={{
+                    toolbar: EditToolbar as GridSlots['toolbar'],
+                }}
+                slotProps={{
+                    toolbar: { setRows, setRowModesModel },
+                }}
             />
         </Box>
     );
--- src/screens/TripEditorScreen.tsx
+++ src/screens/TripEditorScreen.tsx
@@ -5,8 +5,6 @@ import TripsGrid from "../components/TripsGrid";
 
 
 export const TripEditorScreen = () => {
-    const {trips} = useTripContext();
-    console.log(trips);
     // Set the document title when component mounts
     useEffect(() => {
         document.title = "Travel Planner - Editor";
